3b0d27f8638eca9f0b74f5b860a35f841ff4c17d
Vipin Singh <vipinsingh211@gmail.com>
2024-01-13 08:36:57 +0000

    basic layout
    - 3 independently scrollable divs
    - initial layout
    - middle div is a textarea

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# notedown
+# Notedown (WORK IN PROGRESS)
+
 A notes app based on Markdown
+
+
+### Setup and build instructions
+
+https://www.electronjs.org/docs/latest/tutorial/quick-start#recap
---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
